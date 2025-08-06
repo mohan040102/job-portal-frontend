29a3cfc770c54ff346811f002251f9ce1f982c46
mohan040102 <mohanasubbu4102@gmail.com>
2025-08-06 16:34:18 +0530

    update the backend url

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
     project_key: "Job-Portal",
-    api_base_url: "https://job-portal-backend-94h7.onrender.com"
+    api_base_url: "https://job-portal-backend-production-72a3.up.railway.app"
 }
 
 export default config;
